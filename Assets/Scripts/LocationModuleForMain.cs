81223fbb407ee45c3f13efb2598e9216ce027687
inticoy <inticoy0406@gmail.com>
2023-08-10 00:29:39 +0900

    Feat: MainSceneNew with cubes

---
+++ Assets/Scripts/LocationModuleForMain.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationModuleForMain : MonoBehaviour
+{
+    public double latitude, longitude, altitude;
+    public bool isLocationModuleReady;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isLocationModuleReady = false;
+        Input.location.Start(0.1f, 0.1f);
+
+        if (Input.location.isEnabledByUser)
+        {
+            StartCoroutine(InitializeGPS());
+        }
+    }
+
+    IEnumerator InitializeGPS()
+    {
+        // 위치 서비스 초기화 중일 때까지 대기
+        while (Input.location.status == LocationServiceStatus.Initializing)
+        {
+            yield return new WaitForSeconds(1);
+        }
+
+        // 위치 서비스 초기화가 성공한 경우
+        if (Input.location.status == LocationServiceStatus.Running)
+        {
+            // GPS 데이터 갱신 시작            
+            StartCoroutine(UpdateGPSData());
+        }
+        else
+        {
+            Debug.Log("Failed to initialize GPS");
+        }
+    }
+
+    IEnumerator UpdateGPSData()
+    {
+        int gps_connect = 0;
+
+        while (true)
+        {
+            // GPS 데이터 업데이트 대기
+            yield return new WaitForSecondsRealtime(1);
+            // 현재 GPS 데이터 가져오기
+            LocationInfo currentGPSPosition = Input.location.lastData;
+
+            // 위도와 경도 텍스트 업데이트
+            gps_connect++;
+
+            latitude = currentGPSPosition.latitude;
+            longitude = currentGPSPosition.longitude;
+            altitude = currentGPSPosition.altitude;
+
+            isLocationModuleReady = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Main/MapMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Maps.Unity;
+using UnityEngine;
+
+public class MapMovement : MonoBehaviour
+{
+    [SerializeField]
+    MapRenderer mapRenderer;
+
+    [SerializeField]
+    LocationModuleForMain locationModule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapRenderer.Center = new Microsoft.Geospatial.LatLon(locationModule.latitude, locationModule.longitude);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //mapRenderer.Center = new Microsoft.Geospatial.LatLon(locationModule.latitude, locationModule.longitude);
+        mapRenderer.Center = new Microsoft.Geospatial.LatLon(37.4885, 127.0655);
+    }
+}
