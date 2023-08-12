cfc1cc46aa5e7fe2c2960559e8cd74b250fd9532
Jukim2 <junseojskim@gmail.com>
2023-08-12 20:18:36 +0900

    Feat: Add Map to RunningAloneScene

---
+++ Assets/Scripts/MapSizeController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSizeController : MonoBehaviour
+{
+    public Microsoft.Maps.Unity.MapRenderer mapRender;
+    private bool isExtended = false;
+    private float contractedScale = 0.42f;
+    private float extendScale = 1.2f;
+
+    public void ChangeSize()
+    {
+        if (isExtended)
+        {
+            transform.localScale = new Vector3(contractedScale, contractedScale, contractedScale);
+            mapRender.ZoomLevel = 18;
+        }
+        else
+        {
+            transform.localScale = new Vector3(extendScale, extendScale, extendScale);
+            mapRender.ZoomLevel = 16;
+        }
+        isExtended = !isExtended;
+    }
+}
