18b05ae0516b7377ba957f98b8428785d9d73f0b
dandyday <dandyday31@gmail.com>
2023-08-15 12:01:42 +0900

    Fix: MainScene

--- Assets/Scripts/Main/StartSceneLoadManager.cs
+++ Assets/Scripts/Main/StartSceneLoadManager.cs
@@ -5,11 +5,11 @@ using Qualcomm.Snapdragon.Spaces.Samples;
 
 public class StartSceneLoadManager : MonoBehaviour
 {
-
+    public Animator manAnimator;
     public GameObject man;
     public float movementStep = 0.001f;
     public int totalSteps = 5000;
-    public float delayTime = 1.0f; // Delay time in seconds
+    public float delayTime = 1.8f; // Delay time in seconds
 
 
     private InteractionManager _interactionManager;
@@ -37,14 +37,30 @@ public class StartSceneLoadManager : MonoBehaviour
 
     private IEnumerator MoveManGradually()
     {
-
+        manAnimator.SetBool("is Run", true);
         for (int i = 0; i < totalSteps; i++)
         {
             //man.transform.position = Vector3.Lerp(transform.position, targetPosition, totalSteps);
 
             man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.forward);
-            man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.up);
-            man.transform.localScale += new Vector3(movementStep, movementStep, movementStep);
+            //man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.up);
+            //man.transform.localScale += new Vector3(movementStep, movementStep, movementStep);
+            yield return null; // Wait for one frame
+        }
+        yield return new WaitForSecondsRealtime(0.5f);
+        for (int i = 0; i < 225; i++)
+        {
+            man.transform.position += 0.001f * Vector3.Normalize(man.transform.forward);
+            man.transform.Rotate(0, -0.4f, 0);
+            yield return null;
+        }
+        //man.transform.Rotate(0, -90, 0);
+        for (int i = 0; i < totalSteps*10; i++)
+        {
+            //man.transform.position = Vector3.Lerp(transform.position, targetPosition, totalSteps);
+            man.transform.position += -0.1f * movementStep * Vector3.Normalize(-man.transform.parent.right);
+            //man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.up);
+            //man.transform.localScale += new Vector3(movementStep, movementStep, movementStep);
             yield return null; // Wait for one frame
         }
 
