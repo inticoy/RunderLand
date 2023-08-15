35a4f3b356da4a53a4944fc94163def50765944d
dandyday <dandyday31@gmail.com>
2023-08-15 14:42:19 +0900

    Fix: MainScene

--- Assets/Scripts/Main/StartSceneLoadManager.cs
+++ Assets/Scripts/Main/StartSceneLoadManager.cs
@@ -8,8 +8,8 @@ public class StartSceneLoadManager : MonoBehaviour
     public Animator manAnimator;
     public GameObject man;
     public float movementStep = 0.005f;
-    public int totalSteps = 1000;
-    public float delayTime = 2.5f; // Delay time in seconds
+    public int totalSteps = 100;
+    public float delayTime = 5f; // Delay time in seconds
 
 
     private InteractionManager _interactionManager;
@@ -27,38 +27,29 @@ public class StartSceneLoadManager : MonoBehaviour
 
     private IEnumerator MoveManGraduallyAndLoad(string sceneName)
     {
-        // Start both coroutines simultaneously
-        StartCoroutine(MoveManGradually());
-        yield return new WaitForSeconds(delayTime);
 
-        _interactionManager?.SendHapticImpulse();
-        SceneManager.LoadScene(sceneName);
-    }
-
-    private IEnumerator MoveManGradually()
-    {
         manAnimator.SetBool("is Run", true);
         for (int i = 0; i < totalSteps; i++)
         {
             //man.transform.position = Vector3.Lerp(transform.position, targetPosition, totalSteps);
 
-            man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.forward);
+            man.transform.position += -1.0f * movementStep * Vector3.Normalize(man.transform.parent.forward);
             //man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.up);
             //man.transform.localScale += new Vector3(movementStep, movementStep, movementStep);
             yield return null; // Wait for one frame
         }
-        yield return new WaitForSecondsRealtime(0.5f);
-        for (int i = 0; i < 45; i++)
+        for (int i = 0; i < 5; i++)
         {
             man.transform.position += 0.001f * Vector3.Normalize(man.transform.forward);
-            man.transform.Rotate(0, -2f, 0);
+            man.transform.Rotate(0, -18f, 0);
             yield return null;
         }
         //man.transform.Rotate(0, -90, 0);
-        for (int i = 0; i < totalSteps*10; i++)
+        StartCoroutine(MoveManGradually(sceneName));
+        for (int i = 0; i < totalSteps * 2; i++)
         {
             //man.transform.position = Vector3.Lerp(transform.position, targetPosition, totalSteps);
-            man.transform.position += -0.1f * movementStep * Vector3.Normalize(-man.transform.parent.right);
+            man.transform.position += -0.5f * movementStep * Vector3.Normalize(-man.transform.parent.right);
             //man.transform.position += -0.25f * movementStep * Vector3.Normalize(man.transform.parent.up);
             //man.transform.localScale += new Vector3(movementStep, movementStep, movementStep);
             yield return null; // Wait for one frame
@@ -66,5 +57,14 @@ public class StartSceneLoadManager : MonoBehaviour
 
         // Ensure that the final position is set correctly
         //man.transform.position = targetPosition;
+        // Start both coroutines simultaneously
+    }
+
+    private IEnumerator MoveManGradually(string sceneName)
+    {
+        yield return new WaitForSecondsRealtime(delayTime);
+
+        _interactionManager?.SendHapticImpulse();
+        SceneManager.LoadScene(sceneName);
     }
 }
