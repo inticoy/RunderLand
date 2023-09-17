07b3c3a59160bb1b835b80af48aff35716e70081
Jukim2 <junseojskim@gmail.com>
2023-09-17 16:53:43 +0900

    Feat: Main Scene Complete

--- Assets/PickAvatar.cs
+++ Assets/PickAvatar.cs
@@ -8,6 +8,28 @@ public class PickAvatar : MonoBehaviour
 
     void Start()
     {
-        
+        if (!PlayerPrefs.HasKey("avatar"))
+        {
+            PlayerPrefs.SetString("avatar", "BicycleMan");            
+        }
+        string avatar = PlayerPrefs.GetString("avatar");
+        if (avatar == "BicycleMan")
+        {
+            AnimeMan.SetActive(false);
+            RealisticMan.SetActive(false);
+            BicycleMan.SetActive(true);
+        }
+        else if (avatar == "AnimeMan")
+        {
+            AnimeMan.SetActive(true);
+            RealisticMan.SetActive(false);
+            BicycleMan.SetActive(false);
+        }
+        else if (avatar == "RealisticMan")
+        {
+            AnimeMan.SetActive(false);
+            RealisticMan.SetActive(true);
+            BicycleMan.SetActive(false);
+        }
     }
 }
